6c1de4ffb2d0930c137cf1fc4887619d55aeafa1
NOBEL118 <nobelvansh118@gmail.com>
2026-08-30 13:44:39 +0530

    improved the system prompt

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get("/", (req, res) => {
 
 app.post("/form", async (req, res) => {
   const data = req.body;
+  console.log(data);
   if (!data) {
     return res.status(400).send("Can't get data");
   }
